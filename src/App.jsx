b791e6634113fd02b0c1054558162f7e6b794b73
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-06 02:00:44 +0900

    Adding logic to render app dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import DisplayWallet from "./components/DisplayWallet";
 import Web3 from "web3";
 import contract from "./js/lottery.js";
+import { useState } from "react";
 
 const App = () => {
-
+  const [showApp, setShowApp] = useState(false);
   const hello = async () => {
     // Check if Metamask is installed
     if (window.ethereum) {
@@ -15,15 +16,18 @@ const App = () => {
 
       // Get the connected accounts
       const accounts = await web3.eth.getAccounts();
-      console.log('Connected accounts:', accounts);
+      console.log("Connected accounts:", accounts);
 
       // Get the manager from the contract
       const manager = await contract.methods.manager().call();
-      console.log('Manager Address:', manager);
-
+      console.log("Manager Address:", manager);
     } else {
-      console.log('MetaMask is not installed');
+      console.log("MetaMask is not installed");
     }
+  };
+
+  const displayApp = () => {
+    setShowApp(true);
   };
 
   // Call hello on component mount
@@ -31,7 +35,7 @@ const App = () => {
     hello();
   }, []);
 
-  return <DisplayWallet />;
+  return <>{showApp ? <h1>Hello</h1> : <DisplayWallet displayApp={displayApp} />}</>;
 };
 
 export default App;
