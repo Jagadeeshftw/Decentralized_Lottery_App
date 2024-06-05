9408ae683f3ed71c122401bbdaf9030138dc0c90
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-05 22:42:19 +0900

    Removing unused modules

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import "./App.css";
 import React from "react";
 import DisplayWallet from "./components/DisplayWallet";
 import Web3 from "web3";
-import { useSyncProviders } from "./hooks/useSyncProviders";
 import contract from "./js/lottery.js";
 
 const App = () => {
-  const providers = useSyncProviders();
-  let currentAccount = null;
 
   const hello = async () => {
     // Check if Metamask is installed
@@ -20,15 +17,10 @@ const App = () => {
       const accounts = await web3.eth.getAccounts();
       console.log('Connected accounts:', accounts);
 
-      // Ensure the contract address is correctly set
-      console.log('Contract:', contract);
-
       // Get the manager from the contract
       const manager = await contract.methods.manager().call();
       console.log('Manager Address:', manager);
 
-      const address = contract.options.address;
-      console.log('Contract Address:', address);
     } else {
       console.log('MetaMask is not installed');
     }
