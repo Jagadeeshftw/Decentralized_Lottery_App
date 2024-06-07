dfa71f35557ef83cad2d8bca307dd91e8b63e363
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-08 01:16:50 +0530

    Adding new Contract Address

--- src/js/lottery.js
+++ src/js/lottery.js
@@ -5,7 +5,7 @@ const web3 = new Web3(window.ethereum);
 const contract = new web3.eth.Contract(
   abi,
   //   "0xF81413BeF8a63b41E1c262B729Df3E75548A8441"
-  "0xB984CB3a1cfB4DBdd9D16202c4213628D4F4E97E"
+  "0x38223BEA0BBcfC520530e442b333AC73779248f5"
 );
 
 export default contract;
