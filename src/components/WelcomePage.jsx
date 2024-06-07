000882fccda789503830346c82875ce5ec64b800
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-07 20:19:32 +0530

    Rendering values via Context

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import lottery from "../assets/Lottery-machine.png";
-const WelcomePage = ({ currentBalance, currentPlayers }) => {
+import { useContext } from "react";
+import { LotteryContext } from "../store/Lottery-context";
+const WelcomePage = () => {
+
+  const {currentBalance, currentPlayers} = useContext(LotteryContext)
   return (
     <section
       className="hero section bg-dark text-secondary px-4 py-5 text-center"
       style={{ minHeight: "100vh" }}
     >
-      <div class="container">
-        <div class="row gy-4 d-flex justify-content-between">
-          <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+      <div className="container">
+        <div className="row gy-4 d-flex justify-content-between">
+          <div className="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
             <h2 className="display-5 fw-bold text-white" data-aos="fade-up">
               Welcome to the Decentralized Lottery
             </h2>
@@ -17,30 +21,40 @@ const WelcomePage = ({ currentBalance, currentPlayers }) => {
               and fairness are guaranteed. Connect your wallet and get started!
             </p>
 
+
+          {/* <div className="selectedWallet mt-4">
+            <img
+              src={selectedWallet?.info.icon}
+              alt={selectedWallet?.info.name}
+            />
+            <div>{selectedWallet?.info.name}</div>
+            <div>({formatAddress(userAccount)})</div>
+          </div> */}
+
             <form
               action="#"
-              class="form-search d-flex align-items-stretch mb-3"
+              className="form-search d-flex align-items-stretch mb-3"
               data-aos="fade-up"
               data-aos-delay="200"
             >
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="ETH Address"
               />
-              <button type="submit" class="btn btn-primary">
+              <button type="submit" className="btn btn-primary">
                 Enter
               </button>
             </form>
 
-            <div class="row gy-4" data-aos="fade-up" data-aos-delay="300">
-              <div class="col-lg-6 col-6">
-                <div class="stats-item text-center w-100 h-100">
+            <div className="row gy-4" data-aos="fade-up" data-aos-delay="300">
+              <div className="col-lg-6 col-6">
+                <div className="stats-item text-center w-100 h-100">
                   <span
                     data-purecounter-start="0"
                     data-purecounter-end="232"
                     data-purecounter-duration="0"
-                    class="purecounter"
+                    className="purecounter"
                   >
                     {currentPlayers.length}
                   </span>
@@ -48,13 +62,13 @@ const WelcomePage = ({ currentBalance, currentPlayers }) => {
                 </div>
               </div>
 
-              <div class="col-lg-6 col-6">
-                <div class="stats-item text-center w-100 h-100">
+              <div className="col-lg-6 col-6">
+                <div className="stats-item text-center w-100 h-100">
                   <span
                     data-purecounter-start="0"
                     data-purecounter-end="521"
                     data-purecounter-duration="0"
-                    class="purecounter"
+                    className="purecounter"
                   >
                     {currentBalance} ETH
                   </span>
@@ -63,16 +77,18 @@ const WelcomePage = ({ currentBalance, currentPlayers }) => {
               </div>
             </div>
           </div>
-          <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out">
-            <div class="row">
+          <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out">
+            <div className="row">
               <div className="col-12">
                 <img
                   src={lottery}
-                  class="img-fluid"
+                  className="img-fluid"
                   style={{ width: "100%" }}
                   alt=""
                 />
-                <a class="cta-btn" href="#">Pick Winner</a>
+                <a className="cta-btn" href="#">
+                  Pick Winner
+                </a>
               </div>
             </div>
           </div>
