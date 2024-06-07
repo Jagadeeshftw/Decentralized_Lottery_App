e014d9d9b10247f5e5ca0bc287d324ff58aca4fa
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-07 20:19:59 +0530

    Adding Context Provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import App from './App.jsx'
 import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { LotteryContextProvider } from './store/Lottery-context.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <LotteryContextProvider >
     <App />
+    </LotteryContextProvider>
 )
