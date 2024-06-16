198bcdbe1b725b4068b9c73b78051a5848270231
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-17 00:45:11 +0530

    Fixing undefined bug

--- src/js/lottery.js
+++ src/js/lottery.js
@@ -1,11 +1,16 @@
+"use client";
 import Web3 from "web3";
 import abi from "../build/LotteryABI.json";
-const web3 = new Web3(window.ethereum);
 
-const contract = new web3.eth.Contract(
-  abi,
-  //   "0xF81413BeF8a63b41E1c262B729Df3E75548A8441"
-  "0x38223BEA0BBcfC520530e442b333AC73779248f5"
-);
+let contract = undefined;
+if (typeof window !== "undefined" && window.ethereum) {
+  const web3 = new Web3(window.ethereum);
+
+  contract = new web3.eth.Contract(
+    abi,
+    //   "0xF81413BeF8a63b41E1c262B729Df3E75548A8441"
+    "0x38223BEA0BBcfC520530e442b333AC73779248f5"
+  );
+}
 
 export default contract;
