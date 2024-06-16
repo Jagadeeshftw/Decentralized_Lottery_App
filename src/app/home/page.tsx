dc43835486c0acf15300fc74e642ba21b4d408b5
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-17 01:25:50 +0530

    Improving code alignments and quality

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import lottery from "../icon.png";
 import { LotteryContext } from "../../store/Lottery-context";
 import contract from "../../js/lottery";
 import Web3 from "web3";
+import Image from "next/image";
 
 const WelcomePage = () => {
   const {
@@ -46,8 +47,6 @@ const WelcomePage = () => {
       // Get current players
       const players = await contract.methods.getPlayers().call();
       handleCurrentPlayers(players);
-      console.log("the current players " + currentPlayers);
-      console.log("the current balance " + currentBalance);
       // Get current balance
       const balance = await web3.eth.getBalance(contract.options.address);
       handleCurrentBalance(parseFloat(web3.utils.fromWei(balance, "ether")));
@@ -61,10 +60,10 @@ const WelcomePage = () => {
 
   return (
     <section
-      className="hero section bg-dark text-secondary px-0 py-5 text-center"
+      className="hero section bg-dark text-secondary py-5 px-5 text-center"
       style={{ minHeight: "100vh" }}
     >
-      <div className="container">
+      <div className="container-fluid">
         <div className="row d-flex justify-content-between">
           <div className="col-lg-7 order-2 order-lg-1 d-flex flex-column justify-content-center">
             <h4 className="display-6 fw-bold text-white">
@@ -76,7 +75,7 @@ const WelcomePage = () => {
               fee is 0.001 ETH. Stay tuned for updates on the total participants
               and current prize pool.
             </p>
-            <form onSubmit={handleSubmit} className="d-flex  py-2 m-0">
+            <form onSubmit={handleSubmit} className="d-flex py-2">
               <div className="form-floating" style={{ flex: "5" }}>
                 <input
                   type="text"
@@ -132,14 +131,8 @@ const WelcomePage = () => {
             </div>
           </div>
 
-          <div className="col-lg-5 order-1 order-lg-2 hero-img">
-            <img
-              src={lottery.src}
-              className="img-fluid"
-              style={{ width: "100%" }}
-              alt=""
-            />
-
+          <div className="col-lg-5 order-1 order-lg-2 d-flex flex-column align-items-center justify-content-center">
+            <Image src={lottery} alt="" width={600} height={350} />
             <a className="cta-btn" href="#">
               Pick Winner
             </a>
