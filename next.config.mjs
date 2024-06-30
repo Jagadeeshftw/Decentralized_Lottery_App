07ad1c3196c3b4c36d3b756fdc033a3ec59dc4ac
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-30 19:38:27 +0530

    Adding Sass config

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,8 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add a resolve.alias configuration to the existing Webpack config
-    config.resolve.alias["~bootstrap"] = path.resolve(
-      __dirname,
-      "node_modules/bootstrap"
-    );
-
-    // Important: return the modified config
-    return config;
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
