22d105982222fb8eca2afe8b99391145f3ba21c4
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-08 12:54:22 +0530

    Updating lottery contract address

--- src/js/lottery.js
+++ src/js/lottery.js
@@ -9,7 +9,8 @@ if (typeof window !== "undefined" && window.ethereum) {
   contract = new web3.eth.Contract(
     abi,
     //   "0xF81413BeF8a63b41E1c262B729Df3E75548A8441"
-    "0x38223BEA0BBcfC520530e442b333AC73779248f5"
+    //"0x38223BEA0BBcfC520530e442b333AC73779248f5"
+    "0xBE1809D4b709A506cf857Bf7A47A98184246D278"
   );
 }
 
